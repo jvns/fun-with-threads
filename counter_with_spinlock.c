a7a5a0d0cddf9ab9e414d2ab314eedb8af8fa6ce
Julia Evans <julia@jvns.ca>
2014-12-13 15:52:53 -0500

    add pthread spinlock

---
+++ counter_with_spinlock.c
@@ -0,0 +1,36 @@
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#define NUM_THREADS     20
+#define NUM_INCREMENTS  1000000000
+
+int counter;
+pthread_spinlock_t spinlock;
+
+
+void *AddThings(void *threadid)
+{
+   for (int i = 0; i < NUM_INCREMENTS; i++) {
+       pthread_spin_lock(&spinlock);
+       counter += 1;
+       pthread_spin_unlock(&spinlock);
+   }
+   pthread_exit(NULL);
+}
+
+int main (int argc, char *argv[])
+{
+   pthread_t threads[NUM_THREADS];
+   long t;
+   for(t=0; t<NUM_THREADS; t++){
+      int rc = pthread_create(&threads[t], NULL, AddThings, (void *)t);
+      if (rc){
+         printf("ERROR; return code from pthread_create() is %d\n", rc);
+         exit(1);
+      }
+   }
+   for (t = 0; t < NUM_THREADS; t++)
+       pthread_join(threads[t], NULL);
+   printf("Final value of counter is: %d\n", counter);
+   pthread_exit(NULL);
+}
--- rust_counter_atomics.rs
+++ rust_counter_atomics.rs
@@ -11,12 +11,12 @@ fn main() {
         let (counter, tx) = (counter.clone(), tx.clone());
         spawn(proc() {
             for _ in range(0u, NUM_INCREMENTS) {
-                counter.fetch_add(1, Relaxed)
+                counter.fetch_add(1, Relaxed);
             }
             tx.send(());
         })
     }
     // Wait for threads to finish
     for _ in range(0u, NUM_THREADS) { rx.recv(); }
-    println!("{}" , counter.load(SeqCst));
+    println!("{}" , counter.load(Relaxed));
 }
