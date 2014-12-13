dedd7073eb0b10612499a52515b0082ff998d1b6
Julia Evans <julia@jvns.ca>
2014-12-13 14:17:21 -0500

    whitespace fixes

--- rust_counter.rs
+++ rust_counter.rs
@@ -3,16 +3,17 @@ const NUM_THREADS: uint = 20;
 const NUM_INCREMENTS: uint = 100000u;
 
 fn main() {
-
     let data = Arc::new(Mutex::new(0u));
-    let (tx, rx): (Sender<()>, Receiver<()>) = channel();
+    let (tx, rx) = channel();
     for _ in range(0u, NUM_THREADS) {
         let (data, tx) = (data.clone(), tx.clone());
         spawn(proc() {
-              for _ in range(0u, NUM_INCREMENTS) {
-                  let mut d = data.lock();
-                  *d += 1;
-              } tx.send(()); })
+            for _ in range(0u, NUM_INCREMENTS) {
+                let mut d = data.lock();
+                *d += 1;
+            }
+            tx.send(());
+        })
     }
     for _ in range(0u, NUM_THREADS) { rx.recv(); }
     let d = data.lock();
